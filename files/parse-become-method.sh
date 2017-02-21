68267fcd296a7e5a7e0c801e079235e2b3d74cfb
James Z.M. Gao <gaozhiming@360.cn>
2017-02-21 17:57:18 +0800

    assert ssh_executable should be overriden

--- files/parse-become-method.sh
+++ files/parse-become-method.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-while test ":$#" != ":0" && test ":$1" != ":BECOME-HOST"; do
+last=$1
+shift
+for i; do
+  last="$1"
   shift
 done
 
-set -- $2
-if test -n "$1"; then
-  echo -n "$1"
-  exit 0
+if test ":$last" = ":--FAKE::::COMMAND=$PPID"; then
+  echo -n NO-BECOME
+else
+  set -- $last
+  echo -n "${1:-sudo}"
 fi
-
-echo -n sudo
