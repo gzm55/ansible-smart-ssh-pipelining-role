dcc52f07aa70d4620f2d8f34e2129382626b8c1f
James Z.M. Gao <gaozm55@gmail.com>
2023-06-20 15:36:07 +0800

    use ConfigManager().get_config_value_and_origin() to get config origin

--- action_plugins/pipelining_config_origin.py
+++ action_plugins/pipelining_config_origin.py
@@ -15,7 +15,8 @@ class ActionModule(ActionBase):
         result = super(ActionModule, self).run(tmp, task_vars)
         del tmp  # tmp no longer has any effect
 
-        pipelining_settting = ConfigManager().data.get_setting('ANSIBLE_PIPELINING')
-        result['msg'] = pipelining_settting.origin if pipelining_settting else ''
+        _value, origin = ConfigManager().get_config_value_and_origin('ANSIBLE_PIPELINING', variables=task_vars)
+        # pipelining_settting = ConfigManager().data.get_setting('ANSIBLE_PIPELINING')
+        result['msg'] = origin if origin is not None else ''
         result['changed'] = False
         return result
